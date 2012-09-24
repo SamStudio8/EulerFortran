0f1bc67eaf213e5fe77fca8ee11886f6b559da0a
Sam Nicholls <sam.n@studio8media.co.uk>
2012-09-24 19:15:02 +0100

    Tidied Problem 6

--- problem6.f90
+++ problem6.f90
@@ -1,12 +1,12 @@
 program problem6
-        integer::i,total1,total2
-        
-        total1 = 0
-        total2 = 0
+    integer::i,sum_squares,total_sum
+    
+    sum_squares = 0
+    total_sum = 0
 
-        do i=1,100
-                total1 = total1 + i**2
-                total2 = total2 + i
-        end do
-        write(*,*) total2**2 - total1
+    do i=1,100
+        sum_squares = sum_squares + i**2
+        total_sum = total_sum + i
+    end do
+    write(*,*) total_sum**2 - sum_squares
 end program
