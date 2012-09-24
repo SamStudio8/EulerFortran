fb8ccf7f046a4bfb7d332a71101029456081cd16
Sam Nicholls <sam.n@studio8media.co.uk>
2012-09-24 20:37:51 +0100

    is_prime function

--- problem10.f90
+++ problem10.f90
@@ -1,5 +1,26 @@
+pure function is_prime(i)
+    integer, intent(in)::i
+    integer::d
+
+    is_prime = 0
+    if(mod(i,2) == 0) then
+        return
+    end if
+    d = 3
+    do while(d <= ceiling(sqrt(real(i))))
+        if(mod(i,d) == 0) then
+            return
+        end if
+        d = d+2
+    end do
+
+    is_prime = 1
+    return
+end function
+
 program problem10
-    integer::i,divisible,d,limit
+    integer::i,limit
+    integer::is_prime
     integer(kind=8)::sum_primes
 
     i = 2
@@ -8,21 +29,7 @@ program problem10
 
     do while(i < limit)
         i = i + 1
-        if(mod(i,2) == 0) then
-            cycle
-        end if
-
-        divisible = 0
-        d = 3
-        do while(d <= ceiling(sqrt(real(i))))
-            if(mod(i,d) == 0) then
-                divisible = 1
-                exit
-            end if
-            d = d+2
-        end do
-
-        if(divisible == 0) then
+        if(is_prime(i) == 1) then
             sum_primes = sum_primes + i
         end if
     end do
--- problem7.f90
+++ problem7.f90
@@ -1,26 +1,33 @@
+pure function is_prime(i)
+    integer, intent(in)::i
+    integer::d
+
+    is_prime = 0
+    if(mod(i,2) == 0) then
+        return
+    end if
+    d = 3
+    do while(d <= ceiling(sqrt(real(i))))
+        if(mod(i,d) == 0) then
+            return
+        end if
+        d = d+2
+    end do
+    
+    is_prime = 1
+    return
+end function
+
 program problem7
-    integer::i,ul,counter,d,divisible
+    integer::i,ul,counter
+    integer::is_prime
     i = 2
     counter = 1
     ul = 10001
 
     do while (counter /= ul)
         i = i + 1
-        if(mod(i,2) == 0) then
-            cycle
-        end if
-
-        divisible = 0
-        d = 3
-        do while(d <= ceiling(sqrt(real(i))))
-            if(mod(i,d) == 0) then
-                divisible = 1
-                exit
-            end if
-            d = d+2
-        end do
-
-        if(divisible == 0) then
+        if(is_prime(i) == 1) then
             counter = counter + 1
         end if
     end do
